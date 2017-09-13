7b4190b1f8a564ba0ea80101e651b542371a36ae
Eduardo <hernandezed.1991@gmail.com>
2017-09-13 14:10:53 -0300

    add new configuration to mariadb4jAutoConfiguration add jdbc starter

--- MariaDB4j-Spring-Boot-Autoconfiguration/src/main/java/com/spring/boot/mariadb4j/autoconfiguration/MariaDB4jAutoConfiguration.java
+++ MariaDB4j-Spring-Boot-Autoconfiguration/src/main/java/com/spring/boot/mariadb4j/autoconfiguration/MariaDB4jAutoConfiguration.java
@@ -9,46 +9,39 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.util.StringUtils;
 
 @Configuration
 @ConditionalOnClass({MariaDB4jSpringService.class, DataSource.class})
-@EnableConfigurationProperties(value = {DatasourceProperties.class, MariaDB4jProperties.class})
+@EnableConfigurationProperties(value = {MariaDB4jProperties.class})
 public class MariaDB4jAutoConfiguration {
 
-    private static final String DEFAULT_DRIVER_CLASS_NAME = "org.mariadb.jdbc.Driver";
-    private static final String DEFAULT_DATABASE_NAME = "test";
-    private static final String DEFAULT_USERNAME = "su";
-    private static final String DEFAULT_PASSWORD = "";
-
-    private static final String DEFAULT_DATA_DIR = null;
-    private static final String DEFAULT_LIB_DIR = null;
-    private static final Integer DEFAULT_PORT = 0;
-    private static final String DEFAULT_SOCKET = "";
-
-    private final DatasourceProperties datasourceProperties;
     private final MariaDB4jProperties mariaDB4jProperties;
     private static Log log = LogFactory.getLog(MariaDB4jAutoConfiguration.class);
 
-    public MariaDB4jAutoConfiguration(DatasourceProperties datasourceProperties, MariaDB4jProperties mariaDB4jProperties) {
-        this.datasourceProperties = datasourceProperties;
+    public MariaDB4jAutoConfiguration(MariaDB4jProperties mariaDB4jProperties) {
         this.mariaDB4jProperties = mariaDB4jProperties;
     }
 
     @Bean
     @ConditionalOnMissingBean
     public MariaDB4jSpringService mariaDB4jSpringService() {
-        String dataDir = (StringUtils.isEmpty(mariaDB4jProperties.getDataDir())) ? DEFAULT_DATA_DIR : mariaDB4jProperties.getDataDir();
-        String libDir = (StringUtils.isEmpty(mariaDB4jProperties.getLibDir())) ? DEFAULT_LIB_DIR : mariaDB4jProperties.getLibDir();
-        String socket = (StringUtils.isEmpty(mariaDB4jProperties.getSocket())) ? DEFAULT_SOCKET : mariaDB4jProperties.getSocket();
-        Integer port = (mariaDB4jProperties.getPort() == null) ? DEFAULT_PORT : mariaDB4jProperties.getPort();
         MariaDB4jSpringService mariaDB4jSpringService = new MariaDB4jSpringService();
-        if (mariaDB4jProperties.getBaseDir() != null) {
-            mariaDB4jSpringService.setDefaultBaseDir(mariaDB4jProperties.getBaseDir());
-        }
+        DBConfigurationBuilder dBConfigurationBuilder = mariaDB4jSpringService.getConfiguration();
+
+        String dataDir = (StringUtils.isEmpty(mariaDB4jProperties.getDataDir())) ? dBConfigurationBuilder.getDataDir() : mariaDB4jProperties.getDataDir();
+        String libDir = (StringUtils.isEmpty(mariaDB4jProperties.getLibDir())) ? dBConfigurationBuilder.getLibDir() : mariaDB4jProperties.getLibDir();
+        String socket = (StringUtils.isEmpty(mariaDB4jProperties.getSocket())) ? dBConfigurationBuilder.getSocket() : mariaDB4jProperties.getSocket();
+        Integer port = (mariaDB4jProperties.getPort() == null) ? dBConfigurationBuilder.getPort() : mariaDB4jProperties.getPort();
+        String baseDir = (StringUtils.isEmpty(mariaDB4jProperties.getBaseDir())) ? dBConfigurationBuilder.getBaseDir() : mariaDB4jProperties.getBaseDir();
+
+        mariaDB4jSpringService.setDefaultBaseDir(baseDir);
         mariaDB4jSpringService.setDefaultDataDir(dataDir);
         mariaDB4jSpringService.setDefaultLibDir(libDir);
         mariaDB4jSpringService.setDefaultPort(port);
@@ -58,26 +51,26 @@ public class MariaDB4jAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public DataSource dataSource() throws ManagedProcessException {
-        mariaDB4jSpringService().getDB().createDB(mariaDB4jProperties.getDatabaseName());
-        DBConfigurationBuilder dBConfiguration = mariaDB4jSpringService().getConfiguration();
-
+    @DependsOn("mariaDB4jSpringService")
+    public DataSource dataSource(DataSourceProperties dataSourceProperties, MariaDB4jSpringService mariaDB4jSpringService) throws ManagedProcessException {
         String databaseName = mariaDB4jProperties.getDatabaseName();
-        String username = datasourceProperties.getUsername();
-        String password = datasourceProperties.getPassword();
-        String driverClassName = datasourceProperties.getDriverClassName();
-
-        driverClassName = (StringUtils.isEmpty(driverClassName)) ? DEFAULT_DRIVER_CLASS_NAME : driverClassName;
-        databaseName = (StringUtils.isEmpty(databaseName)) ? DEFAULT_DATABASE_NAME : databaseName;
-        username = (StringUtils.isEmpty(username)) ? DEFAULT_USERNAME : username;
-        password = (StringUtils.isEmpty(password)) ? DEFAULT_PASSWORD : password;
+        mariaDB4jSpringService.getDB().createDB(databaseName);
+        DBConfigurationBuilder dBConfiguration = mariaDB4jSpringService().getConfiguration();
 
         return DataSourceBuilder
                 .create()
                 .url(dBConfiguration.getURL(databaseName))
-                .driverClassName(driverClassName)
-                .username(username)
-                .password(password)
+                .driverClassName(dataSourceProperties.determineDriverClassName())
+                .username(dataSourceProperties.determineUsername())
+                .password(dataSourceProperties.determinePassword())
                 .build();
+    }
+
+    @Bean
+    @DependsOn("dataSource")
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        DataSourceTransactionManager manager = new DataSourceTransactionManager();
+        manager.setDataSource(dataSource);
+        return manager;
     }
 }
